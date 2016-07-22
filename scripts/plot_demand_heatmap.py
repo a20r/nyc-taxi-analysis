5697278e6587f9a8b201fb44c52abd0ebebd44d3
Alexander Wallar <alex@wallar.me>
2016-07-22 02:09:48 -0400

    Gotta clean up the profile script

--- scripts/plot_demand_heatmap.py
+++ scripts/plot_demand_heatmap.py
@@ -6,9 +6,9 @@ from folium import plugins
 
 
 def generate_demand_heatmap():
-    df = common.load_data(nrows=10000000)
-    start_dt = datetime(2014, 1, 10, 1, 0)
-    end_dt = datetime(2014, 1, 10, 2, 0)
+    df = common.load_data(nrows=1000000)
+    start_dt = datetime(2014, 1, 10, 4, 0)
+    end_dt = datetime(2014, 1, 10, 4, 10)
     df = common.query_dates(df, start_dt, end_dt, "dropoff_datetime")
     arr = common.get_dropoff_geos(df)
     hmap = folium.Map(location=[40.760096, -73.978844], zoom_start=12)
